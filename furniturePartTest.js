f27af2c01a3453553f82028943e8299b0db50cb5
Naiuhz <naiuhz@gmail.com>
2020-06-04 18:13:39 -0400

    furniturePartTest.js: Calculated monthSigninRewards (furniture parts) and total number of sanity and runs needed to achieve goal

--- furniturePartTest.js
+++ furniturePartTest.js
@@ -2,6 +2,7 @@
 
 const data = require('./data.json')
 const income = require('./currency_income.json')
+const constants = require('./constants.json')
 const timeFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }
 const furniturePartGoal = data.furniture.goalFurnitureParts
 const furniturePartGoalDate = new Date((new Date(data.furniture.goalDate)).setHours(4)) //data.furniture.goalDate
@@ -14,12 +15,11 @@ const timeDiffDays = Math.floor(timeDiff/(1000 * 60 * 60 * 24))
 const timeDiffHours = Math.floor(timeDiff/(1000 * 60 * 60))%24
 const timeDiffMinutes = Math.floor(timeDiff/(1000 * 60))%60
 
+// Calculate number of days and weeks between today and date of goal
 const days = timeDiffDays
-var weeks //= Math.ceil(timeDiffDays/7)
-var monthlySignin = 0
+var weeks = 0
 var UTCMinus7Weekday
 var furniturePartGoalWeekday
-
 UTCMinus7Weekday = UTCMinus7.getDay()
 furniturePartGoalWeekday = furniturePartGoalDate.getDay()
 if (UTCMinus7.getHours < 4) {
@@ -34,28 +34,84 @@ if (furniturePartGoalWeekday == 0){
 }
 
 if (furniturePartGoalWeekday > UTCMinus7Weekday){
-    console.log("Case A")
+    //console.log("Case A")
     weeks = Math.floor(days/7)
 }
 
 if (furniturePartGoalWeekday <= UTCMinus7Weekday){
-    console.log("Case B")
+    //console.log("Case B")
     weeks = Math.ceil(days/7)
 }
 
-if (currentTime.getDay() == 1 && currentTime.getUTCHours >= 4) {
-    console.log("Today is Monday")
+/*if (currentTime.getDay() == 1 && currentTime.getUTCHours >= 4) {
+    //console.log("Today is Monday")
 } else {
-    console.log("Today is " + UTCMinus7.getDay())
-}
+    //console.log("Today is " + UTCMinus7.getDay())
+}*/
 
 console.log("UTC-7: " + UTCMinus7.toLocaleDateString("en-US", timeFormat))
-console.log("Your furniture part goal is: " + furniturePartGoal)
-console.log("Your furniture part goal date is: " + furniturePartGoalDate.toLocaleDateString("en-US", timeFormat))
+//console.log("Your furniture part goal is: " + furniturePartGoal)
+//console.log("Your furniture part goal date is: " + furniturePartGoalDate.toLocaleDateString("en-US", timeFormat))
 console.log("You have " + timeDiffDays + " days, " + timeDiffHours +" hours and " + timeDiffMinutes + " minutes to reach your goal.")
-
 console.log("That's " + days + " days and " + weeks + " weeks.")
 
-// Calculate 
 
-console.log("Calculation: " + income.daily.furnitureParts + "*" + days + " (daily income) + " + income.weekly.furnitureParts + "*" + weeks + " (weekly income) + " + monthlySignin + " (monthly signin income)")
+// Calculate relevant monthly signin rewards
+var monthlySigninFurniturePartIncome = 0
+var monthlySigninFurniturePartCalculation = ""
+var i = 0
+if (data.optional.signinRewardDay) {
+    i = data.optional.signinRewardDay
+}
+for (i += 1 ; i < furniturePartGoalDate.getDate(); i++){
+    if (i in income.monthly){
+        const signinReward = income.monthly[i].split(" ")
+        const rewardQuantity = signinReward[0]
+        const rewardType = signinReward[1]
+        if (rewardType == "FURNITURE_PARTS") {
+            //console.log(rewardQuantity +  " : " + rewardType)
+            monthlySigninFurniturePartCalculation += rewardQuantity + "+"
+            monthlySigninFurniturePartIncome += parseInt(rewardQuantity)
+        }
+    }
+}
+
+monthlySigninFurniturePartCalculation = monthlySigninFurniturePartCalculation.substring(0, monthlySigninFurniturePartCalculation.length - 1)
+
+console.log("Calculation: " + income.daily.furnitureParts + "*" + days + " (daily income) + " + income.weekly.furnitureParts + "*" + weeks + " (weekly income) + " + monthlySigninFurniturePartCalculation + " (monthly signin income)")
+const totalFurniturePartIncome = (income.daily.furnitureParts * days) + (income.weekly.furnitureParts * weeks) + monthlySigninFurniturePartIncome
+console.log("Total furniture part income: " + totalFurniturePartIncome)
+
+const furniturePartFarm = totalFurniturePartIncome - data.furniture.currentFurnitureParts
+
+if (furniturePartFarm >= 0) {
+    const skData = constants.map["SK-" + data.optional.highestAutoSK]
+    const skRunFP = parseInt(skData.furnitureParts)
+    const skRuns = Math.ceil(furniturePartFarm/skRunFP) 
+    const skSanity = skRuns*parseInt(skData.sanity)
+    var furniturePartFarmMessage = "You need to farm SK-" + data.optional.highestAutoSK + " at least " + skRuns + " times ("  + skSanity + " sanity) within the next "
+    if (timeDiffDays) {
+        furniturePartFarmMessage += timeDiffDays + " days"
+    }
+    if (timeDiffHours) {
+        if (timeDiffMinutes) {
+            furniturePartFarmMessage += ", "
+        } else {
+            furniturePartFarmMessage += " and "
+        }
+        furniturePartFarmMessage += timeDiffHours + " hours"
+    }
+    if (timeDiffMinutes) {
+        furniturePartFarmMessage += " and "
+        furniturePartFarmMessage += timeDiffMinutes + " minutes"
+    }
+    furniturePartFarmMessage += " before " + furniturePartGoalDate.toLocaleDateString("en-US", timeFormat) + "."
+    console.log (furniturePartFarmMessage)
+    const avgSKRun = parseFloat(skRuns/timeDiffDays).toFixed(2)
+    console.log ("That's an average of " + avgSKRun + " SK-" + data.optional.highestAutoSK + " runs per day.")
+}
+
+//TODO:
+//change printed furniture part goal to local time
+//calculate # of supply days
+//run SK on which days
