43ea9b099a0a62e7aebd4f4ca32a2dd4d668e592
Naiuhz <naiuhz@gmail.com>
2020-06-06 02:09:48 -0400

    furniturePartTest.js: Added current local time, local goal time and fixed printing hours when 0 days until goal date

--- furniturePartTest.js
+++ furniturePartTest.js
@@ -4,13 +4,21 @@ const data = require('./data.json')
 const income = require('./currency_income.json')
 const constants = require('./constants.json')
 const timeFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }
-const furniturePartGoal = data.furniture.goalFurnitureParts
 const furniturePartGoalDate = new Date((new Date(data.furniture.goalDate)).setHours(4)) //data.furniture.goalDate
-//const furniturePartGoalDate = new Date(data.furniture.goalDate)
 const currentTime = new Date()
 const UTCMinus7 = new Date (currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), currentTime.getUTCHours() - 7, currentTime.getUTCMinutes(), currentTime.getUTCSeconds(), currentTime.getUTCMilliseconds())
+const UTCMinus7ResetHour = 4
+var localUTC
+if (data.optional.timezoneCity) {
+    localUTC = new Date (currentTime.toLocaleString("en-US", {timeZone: data.optional.timezoneCity})); // Default
+} else {
+    localUTC = new Date (currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), currentTime.getUTCHours() + data.optional.timezoneUTC, currentTime.getUTCMinutes(), currentTime.getUTCSeconds(), currentTime.getUTCMilliseconds())
+}
+const localOffset = Math.floor((UTCMinus7 - localUTC)/(1000 * 60 * 60))
+const localfurniturePartGoalDate = new Date(furniturePartGoalDate.setHours((UTCMinus7ResetHour - localOffset)))
 
-const timeDiff = furniturePartGoalDate - UTCMinus7
+
+const timeDiff = furniturePartGoalDate - localUTC
 const timeDiffDays = Math.floor(timeDiff/(1000 * 60 * 60 * 24))
 const timeDiffHours = Math.floor(timeDiff/(1000 * 60 * 60))%24
 const timeDiffMinutes = Math.floor(timeDiff/(1000 * 60))%60
@@ -34,26 +42,27 @@ if (furniturePartGoalWeekday == 0){
 }
 
 if (furniturePartGoalWeekday > UTCMinus7Weekday){
-    //console.log("Case A")
     weeks = Math.floor(days/7)
 }
 
 if (furniturePartGoalWeekday <= UTCMinus7Weekday){
-    //console.log("Case B")
     weeks = Math.ceil(days/7)
 }
 
-/*if (currentTime.getDay() == 1 && currentTime.getUTCHours >= 4) {
-    //console.log("Today is Monday")
-} else {
-    //console.log("Today is " + UTCMinus7.getDay())
-}*/
-
 console.log("UTC-7: " + UTCMinus7.toLocaleDateString("en-US", timeFormat))
-//console.log("Your furniture part goal is: " + furniturePartGoal)
-//console.log("Your furniture part goal date is: " + furniturePartGoalDate.toLocaleDateString("en-US", timeFormat))
-console.log("You have " + timeDiffDays + " days, " + timeDiffHours +" hours and " + timeDiffMinutes + " minutes to reach your goal.")
-console.log("That's " + days + " days and " + weeks + " weeks.")
+if (data.optional.timezoneCity) { //"America/Vancouver"
+    console.log(data.optional.timezoneCity + ' local time: '+ (new Date(localUTC).toLocaleDateString("en-US", timeFormat)))
+} else {
+    if (data.optional.timezoneUTC != -7) {
+        var UTCMessage = "UTC"
+        if (data.optional.timezoneUTC >= 0) {
+            UTCMessage += "+"
+        }
+        UTCMessage += data.optional.timezoneUTC + ": " + localUTC.toLocaleDateString("en-US", timeFormat)
+        console.log(UTCMessage);
+    }
+}
+console.log("You have " + timeDiffDays + " days, " + timeDiffHours +" hours and " + timeDiffMinutes + " minutes to reach your goal. That's a total of " + days + " days and " + weeks + " weeks.")
 
 
 // Calculate relevant monthly signin rewards
@@ -69,7 +78,6 @@ for (i += 1 ; i < furniturePartGoalDate.getDate(); i++){
         const rewardQuantity = signinReward[0]
         const rewardType = signinReward[1]
         if (rewardType == "FURNITURE_PARTS") {
-            //console.log(rewardQuantity +  " : " + rewardType)
             monthlySigninFurniturePartCalculation += rewardQuantity + "+"
             monthlySigninFurniturePartIncome += parseInt(rewardQuantity)
         }
@@ -91,27 +99,25 @@ if (furniturePartFarm >= 0) {
     const skSanity = skRuns*parseInt(skData.sanity)
     var furniturePartFarmMessage = "You need to farm SK-" + data.optional.highestAutoSK + " at least " + skRuns + " times ("  + skSanity + " sanity) within the next "
     if (timeDiffDays) {
-        furniturePartFarmMessage += timeDiffDays + " days"
-    }
-    if (timeDiffHours) {
+        furniturePartFarmMessage += timeDiffDays + " day(s)"
         if (timeDiffMinutes) {
             furniturePartFarmMessage += ", "
         } else {
             furniturePartFarmMessage += " and "
         }
-        furniturePartFarmMessage += timeDiffHours + " hours"
+    }
+    if (timeDiffHours) {
+        furniturePartFarmMessage += timeDiffHours + " hour(s)"
     }
     if (timeDiffMinutes) {
         furniturePartFarmMessage += " and "
-        furniturePartFarmMessage += timeDiffMinutes + " minutes"
+        furniturePartFarmMessage += timeDiffMinutes + " minute(s)"
     }
-    furniturePartFarmMessage += " before " + furniturePartGoalDate.toLocaleDateString("en-US", timeFormat) + "."
+    furniturePartFarmMessage += " before " + localfurniturePartGoalDate.toLocaleDateString("en-US", timeFormat) + "."
     console.log (furniturePartFarmMessage)
-    const avgSKRun = parseFloat(skRuns/timeDiffDays).toFixed(2)
-    console.log ("That's an average of " + avgSKRun + " SK-" + data.optional.highestAutoSK + " runs per day.")
+    //const avgSKRun = parseFloat(skRuns/timeDiffDays).toFixed(2)
+    //console.log ("That's an average of " + avgSKRun + " SK-" + data.optional.highestAutoSK + " runs per day.")
 }
 
-//TODO:
-//change printed furniture part goal to local time
-//calculate # of supply days
-//run SK on which days
+
+// Scheduler
