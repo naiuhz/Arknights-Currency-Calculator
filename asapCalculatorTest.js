d308ac214053a12efe3a9fdfb389344bc3d88979
Naiuhz <naiuhz@gmail.com>
2020-06-13 19:11:45 -0400

    asapCalculator: Added local bed time and day zero calculations

--- asapCalculatorTest.js
+++ asapCalculatorTest.js
@@ -29,6 +29,94 @@ const expWeeklyIncome = income.weekly.exp;
 const lsEXPReward = constants.map['LS-5'].battleRecordEXP;
 const sanityDaily = 240;
 const sanityDailyConsumption = data.optional.dailySanityConsumption;
+const { currentSanity } = data.optional;
+const { localBedTime } = data.optional;
+
+const currentTime = new Date();
+let localUTC;
+if (data.optional.timezoneCity) {
+  localUTC = new Date(
+    currentTime.toLocaleString('en-US', {
+      timeZone: data.optional.timezoneCity,
+    }),
+  ); // Default
+} else {
+  localUTC = new Date(
+    currentTime.getUTCFullYear(),
+    currentTime.getUTCMonth(),
+    currentTime.getUTCDate(),
+    currentTime.getUTCHours() + data.optional.timezoneUTC,
+    currentTime.getUTCMinutes(),
+    currentTime.getUTCSeconds(),
+    currentTime.getUTCMilliseconds(),
+  );
+}
+
+function getBedTime(givenDate) {
+  const bedTime = data.optional.localBedTime;
+  let bedTimeHour = 0;
+  let bedTimeMinute = 0;
+  let bedTimeDate = givenDate.getDate();
+  let colonIndex = -1;
+  colonIndex = bedTime.indexOf(':');
+
+  function getHour(time) {
+    let hour = -1;
+    if (colonIndex !== -1) {
+      hour = parseInt(time.substring(0, colonIndex), 0);
+    }
+    if (
+      bedTime.toUpperCase().indexOf('AM') !== -1 ||
+      (hour < 12 && hour >= 0)
+    ) {
+      // console.log('AM');
+      bedTimeDate += 1;
+      if (colonIndex === -1) {
+        const amIndex = bedTime.toUpperCase().indexOf('AM');
+        hour = parseInt(time.substring(0, amIndex), 0);
+        if (hour > 12) {
+          console.log('Incorrect bedtime format. Defaulting bedtime to 12AM.');
+          hour = 0;
+        }
+      }
+      if (hour === 12) {
+        hour = 0;
+      }
+    } else if (bedTime.toUpperCase().indexOf('PM') !== -1 || hour >= 12) {
+      // console.log('PM');
+      if (colonIndex === -1) {
+        const pmIndex = bedTime.toUpperCase().indexOf('PM');
+        hour = parseInt(time.substring(0, pmIndex), 0);
+        hour -= 12;
+      }
+      if (hour !== 12) {
+        hour += 12;
+      }
+    } else {
+      console.log('Incorrect bedtime format. Defaulting bedtime to 12AM.');
+      bedTimeDate += 1;
+      hour = 0;
+    }
+    return hour;
+  }
+  function getMinute(time) {
+    let minute = 0;
+    if (colonIndex !== -1) {
+      minute = parseInt(time.substring(colonIndex + 1, 5), 0);
+    }
+    return minute;
+  }
+  bedTimeHour = getHour(bedTime);
+  bedTimeMinute = getMinute(bedTime);
+  return new Date(
+    givenDate.getFullYear(),
+    givenDate.getMonth(),
+    bedTimeDate,
+    bedTimeHour,
+    bedTimeMinute,
+    0,
+  );
+}
 
 function calculateASAPRuns(cumulativeLMD, cumulativeEXP) {
   // Let y represent CE-5 runs
@@ -69,22 +157,81 @@ if (!lmdGoal || !expGoal) {
     console.log('Error! Missing goalOperatorEXP value in data.json');
   }
 } else {
+  // Day 0 Calculations
   let day = 0;
-  let iDate = new Date();
+  let iDate = localUTC;
   let iDateNumber = iDate.getDate();
-  let lmdBaseIncomeHours = 0;
-  let lmdBaseIncomeMinutes = 0;
-  let expBaseIncomeHours = 0;
-  let expBaseIncomeMinutes = 0;
 
-  // TODO: Calculate now until local bedtime for day 0
-  // Count number of available runs for day 0 with current sanity and recoverable sanity
+  let cumulativeLMD = lmdCurrent;
+  let cumulativeEXP = expCurrent;
 
-  for (
-    let cumulativeLMD = lmdCurrent, cumulativeEXP = expCurrent;
-    cumulativeLMD < lmdGoal && cumulativeEXP < expGoal;
-    day += 1
-  ) {
+  const bedTimeDate = getBedTime(localUTC);
+  const timeDiff = bedTimeDate - localUTC;
+  if (timeDiff > 0) {
+    const timeDiffHours = Math.floor(timeDiff / (1000 * 60 * 60)) % 24;
+    const timeDiffMinutes = Math.floor(timeDiff / (1000 * 60)) % 60;
+
+    const sanityRechargedUntilBedTime =
+      timeDiffHours * 10 + Math.floor(timeDiffMinutes / 6);
+    console.log(
+      `Day 0: ${timeDiffHours} hours and ${timeDiffMinutes} minutes until your bedtime at ${localBedTime}, and ${sanityRechargedUntilBedTime} sanity left to recharge.`,
+    );
+    const lmdToEXPRatio =
+      (lmdGoal - cumulativeLMD) /
+      lmdBaseIncome /
+      ((expGoal - cumulativeEXP) / expBaseIncome);
+    const dayZeroRuns =
+      (currentSanity + sanityRechargedUntilBedTime) / ceSanityCost;
+    const dayZeroCERuns = Math.round(
+      (lmdToEXPRatio / (lmdToEXPRatio + 1)) * dayZeroRuns,
+    );
+    const dayZeroLSRuns = Math.round(
+      (1 - lmdToEXPRatio / (lmdToEXPRatio + 1)) * dayZeroRuns,
+    );
+    console.log(`CE-5 runs: ${dayZeroCERuns}`);
+    console.log(`LS-5 runs: ${dayZeroLSRuns}`);
+    if (dayZeroCERuns > 0) {
+      cumulativeLMD += dayZeroCERuns * ceLMDReward;
+    } else {
+      // if (cumulativeLMD < lmdGoal)
+      let lmdBaseIncomeHours = 0;
+      let lmdBaseIncomeMinutes = 0;
+      lmdBaseIncomeHours = ((lmdGoal - cumulativeLMD) / lmdBaseIncome) * 24;
+      lmdBaseIncomeMinutes = (lmdBaseIncomeHours % 1) * 60;
+      // cumulativeLMD = lmdGoal;
+      console.log(
+        `LMD Wait: ${Math.floor(lmdBaseIncomeHours)} hour(s) and ${Math.ceil(
+          lmdBaseIncomeMinutes,
+        )} minute(s).`,
+      );
+    }
+    if (dayZeroLSRuns > 0) {
+      cumulativeEXP += dayZeroLSRuns * lsEXPReward;
+    } else {
+      // if (cumulativeEXP < expGoal)
+      let expBaseIncomeHours = 0;
+      let expBaseIncomeMinutes = 0;
+      expBaseIncomeHours = (lmdGoal - cumulativeEXP) / expBaseIncome;
+      expBaseIncomeMinutes = (expBaseIncomeHours % 1) * 60;
+      // cumulativeEXP = expGoal;
+      console.log(
+        `EXP Wait: ${Math.floor(expBaseIncomeHours)} hour(s) and ${Math.ceil(
+          expBaseIncomeMinutes,
+        )} minute(s).`,
+      );
+    }
+  } else {
+    console.log("It's past your bedtime, nothing left to do today.");
+  }
+  console.log(`Date: ${iDate.toLocaleDateString('en-US', dateFormat)}`);
+  console.log(`End of Day LMD: ${cumulativeLMD}/${lmdGoal}`);
+  console.log(`End of Day EXP: ${cumulativeEXP}/${expGoal}`);
+  // TODO: Show calculations here.
+  console.log('----------------------------------');
+
+  // Day 1+ Calculations
+  day += 1;
+  for (; cumulativeLMD < lmdGoal || cumulativeEXP < expGoal; day += 1) {
     iDate = new Date(iDate.setDate(iDate.getDate() + 1));
     iDateNumber = iDate.getDate();
     console.log(`Day: ${day}`);
@@ -101,9 +248,12 @@ if (!lmdGoal || !expGoal) {
     console.log(`LS-5 runs: ${lsRuns}`);
     if (ceRuns > 0) {
       cumulativeLMD += ceRuns * ceLMDReward;
-    } else {
+    } else if (cumulativeLMD < lmdGoal) {
+      let lmdBaseIncomeHours = 0;
+      let lmdBaseIncomeMinutes = 0;
       lmdBaseIncomeHours = ((lmdGoal - cumulativeLMD) / lmdBaseIncome) * 24;
       lmdBaseIncomeMinutes = (lmdBaseIncomeHours % 1) * 60;
+      cumulativeLMD = lmdGoal;
       console.log(
         `LMD Wait: ${Math.floor(lmdBaseIncomeHours)} hour(s) and ${Math.ceil(
           lmdBaseIncomeMinutes,
@@ -112,9 +262,12 @@ if (!lmdGoal || !expGoal) {
     }
     if (lsRuns > 0) {
       cumulativeEXP += lsRuns * lsEXPReward;
-    } else {
+    } else if (cumulativeEXP < expGoal) {
+      let expBaseIncomeHours = 0;
+      let expBaseIncomeMinutes = 0;
       expBaseIncomeHours = (lmdGoal - cumulativeEXP) / expBaseIncome;
       expBaseIncomeMinutes = (expBaseIncomeHours % 1) * 60;
+      cumulativeEXP = expGoal;
       console.log(
         `EXP Wait: ${Math.floor(expBaseIncomeHours)} hour(s) and ${Math.ceil(
           expBaseIncomeMinutes,
@@ -145,6 +298,7 @@ if (!lmdGoal || !expGoal) {
 
     console.log(`End of Day LMD: ${cumulativeLMD}/${lmdGoal}`);
     console.log(`End of Day EXP: ${cumulativeEXP}/${expGoal}`);
+    // TODO: Show calculations here.
     console.log('----------------------------------');
   }
 }
