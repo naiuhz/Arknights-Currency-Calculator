2d359e998d7092b90095a05b7bc89e57beaab7e3
Naiuhz <naiuhz@gmail.com>
2020-06-13 18:09:08 -0400

    bedTimeTest.js: Added 24 hour clock functionality

--- bedTimeTest.js
+++ bedTimeTest.js
@@ -31,18 +31,37 @@ function getBedTime(givenDate) {
   colonIndex = bedTime.indexOf(':');
 
   function getHour(time) {
-    let hour = 0;
+    let hour = -1;
     if (colonIndex !== -1) {
       hour = parseInt(time.substring(0, colonIndex), 0);
     }
-    if (bedTime.toUpperCase().indexOf('AM') !== -1) {
+    if (
+      bedTime.toUpperCase().indexOf('AM') !== -1 ||
+      (hour < 12 && hour >= 0)
+    ) {
       console.log('AM');
       bedTimeDate += 1;
+      if (colonIndex === -1) {
+        const amIndex = bedTime.toUpperCase().indexOf('AM');
+        hour = parseInt(time.substring(0, amIndex), 0);
+        if (hour > 12) {
+          console.log('Incorrect bedtime format. Defaulting bedtime to 12AM.');
+          hour = 0;
+        }
+      }
       if (hour === 12) {
         hour = 0;
       }
-    } else if (bedTime.toUpperCase().indexOf('PM') !== -1) {
+    } else if (bedTime.toUpperCase().indexOf('PM') !== -1 || hour >= 12) {
       console.log('PM');
+      if (colonIndex === -1) {
+        const pmIndex = bedTime.toUpperCase().indexOf('PM');
+        hour = parseInt(time.substring(0, pmIndex), 0);
+        hour -= 12;
+      }
+      if (hour !== 12) {
+        hour += 12;
+      }
     } else {
       console.log('Incorrect bedtime format. Defaulting bedtime to 12AM.');
       bedTimeDate += 1;
@@ -71,3 +90,14 @@ function getBedTime(givenDate) {
 
 const bedTimeDate = getBedTime(localUTC);
 console.log(`bedTimeDate: ${bedTimeDate}`);
+
+const timeDiff = bedTimeDate - localUTC;
+if (timeDiff > 0) {
+  const timeDiffHours = Math.floor(timeDiff / (1000 * 60 * 60)) % 24;
+  const timeDiffMinutes = Math.floor(timeDiff / (1000 * 60)) % 60;
+  console.log(
+    `Day 0: ${timeDiffHours} hours and ${timeDiffMinutes} minutes left.`,
+  );
+} else {
+  console.log("It's past your bedtime, nothing left to do today.");
+}
