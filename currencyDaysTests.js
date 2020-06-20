f58399671123e34cf979ae8b5f9e2c99bccea876
Naiuhz <naiuhz@gmail.com>
2020-06-20 18:17:40 -0400

    currencyDaysTests.js: Added test file for QA testing currency days calculations.

---
+++ currencyDaysTests.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+// let testOne = {};
+// let testTwo = {};
+// let testThree = {};
+// let testFour = {};
+// let testFive = {};
+
+// Scenario 1 (Good Day/Happy Path)
+
+// Scenario 2
+
+// Scenario 3
+
+// Scenario 4
+
+// Scenario 5
