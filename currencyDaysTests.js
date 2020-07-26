60ba55e4d8b82ea4dfe54f86eaa8c913c48c757a
Naiuhz <naiuhz@gmail.com>
2020-07-26 13:26:24 -0400

    currencyDaysTests.js: Finished unit test scenario 1: happy path

--- currencyDaysTests.js
+++ currencyDaysTests.js
@@ -1,21 +1,84 @@
 #!/usr/bin/env node
 
-// let testOne = {};
-// let testTwo = {};
-// let testThree = {};
-// let testFour = {};
-// let testFive = {};
+const data = require('./json/data.json');
+
+const currentTime = new Date();
+let localUTCTime;
+if (data.optional.timezoneCity) {
+  localUTCTime = new Date(
+    currentTime.toLocaleString('en-US', {
+      timeZone: data.optional.timezoneCity,
+    }),
+  ); // Default
+} else {
+  localUTCTime = new Date(
+    currentTime.getUTCFullYear(),
+    currentTime.getUTCMonth(),
+    currentTime.getUTCDate(),
+    currentTime.getUTCHours() + data.optional.timezoneUTC,
+    currentTime.getUTCMinutes(),
+    currentTime.getUTCSeconds(),
+    currentTime.getUTCMilliseconds(),
+  );
+}
+
+const testOne = {
+  lmdCurrent: 127917,
+  expCurrent: 229000,
+  localUTC: localUTCTime,
+  lmdGoal: 356530,
+  expGoal: 185472,
+  goalDate: '2020 June 30',
+  lmdBaseIncome: 41000,
+  expBaseIncome: 28000,
+  currentSanity: 0,
+  monthlyCardDaysLeft: 0,
+  sanityConsumable: {
+    topEmergencySanityPotion: 0,
+    emergencySanityPotion: 0,
+  },
+};
+// const testTwo = {};
+// const testThree = {};
+// const testFour = {};
+// const testFive = {};
 
 const currencyDaysCalculator = require('./currencyDaysCalculator');
 
 // Scenario 1 (Good Day/Happy Path)
+// A goalDate is given and no map farming is necessary
+function scenarioOne(input) {
+  currencyDaysCalculator.function1();
+  const results = currencyDaysCalculator.calculateCurrencyDays(input);
 
-currencyDaysCalculator.function1();
+  if (results.ceRuns === 0 && results.lsRuns === 0) {
+    console.log(`Scenario 1: Success.`);
+  } else {
+    console.log(`Scenario 1: Failed! Output result number: ${results}`);
+  }
+}
 
 // Scenario 2
+// A goalDate is given and some map farming is necessary
+// No sanity potions consumed
+/* function scenarioTwo(input) {
+  currencyDaysCalculator.function1();
+  const results = currencyDaysCalculator.calculateCurrencyDays(input);
+  if (results === 2) {
+    console.log(`Scenario 2: Success. Output result number: ${results}`);
+  } else {
+    console.log(`Scenario 2: Failed! Output result number: ${results}`);
+  }
+} */
 
 // Scenario 3
+// A goalDate is given and some map farming is necessary
+// Some sanity potions consumed
 
 // Scenario 4
 
 // Scenario 5
+
+// Main
+scenarioOne(testOne);
+// scenarioTwo(testTwo);
